fca4fa6cc8d5ce7905ff14b68bfd4b6645813cf6
damiansd1 <43549705+damiansd1@users.noreply.github.com>
2019-10-21 21:15:40 +0300

    Add files via upload

---
+++ NWW zadanie 1 olimpiada.py
@@ -0,0 +1,41 @@
+"""def NWW (inp):
+    fragmenty = 1
+    Części = []
+    while inp/fragmenty!=0:
+        cz = 2
+        Check = False
+        while Check == True:
+            if (inp/fragmenty)%cz==0:
+               Części.append(cz)
+               fragmenty*cz == fragmenty
+               Check = True
+            else:
+                cz+=1
+            print ("good")
+    return Części"""
+#M = input()
+pr = 1
+x=1
+print ("good")
+Check1 = False
+Check2 = False
+Części1 = []
+pr=12
+Części1 = NWW(pr+1)
+print (Części1[0])
+print ("good")
+#while Check1==True:
+ #   if M%pr==0:
+  #      if M%(pr+1)==0:
+  for i in range(1,(a*b)+1):
+      if i/a == int(i/b):
+          return i
+                    
+                
+                
+
+
+            
+                
+                
+                
