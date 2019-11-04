bf9d4103437f9f69dde19242567d71c86081654f
damiansd1 <43549705+damiansd1@users.noreply.github.com>
2019-11-04 22:11:23 +0300

    Add files via upload

--- pis_dlazaw/Pisarze.py
+++ pis_dlazaw/Pisarze.py
@@ -1,19 +1,39 @@
+from sys import exit
 l = input()
 l = int(l)
 txt = []
-MIt = []
 for P in range(l):
-    txt.append = input()
+    txt.append(input())
 for z in range(len(txt)):
     source = txt[z]
+    source = str(source)
     try:
-        MI = open('Mickiewicz').read()
+        file = open('Mickiewicz.txt', 'r')
+        MI = file.read()
     finally: 
-        ('Mickiewicz').close()
-    for i in range(len(MI.split())):        
-        for p in range(len(source.split)):
-            if source.split(p)==MI.split(i):
+        file.close()
+    MI = str(MI)
+    MI = MI.split()
+    source = str(source)
+    source = source.split()
+    for p in range(len(MI)-len(source)):
+        if source[0]== MI[p]:
+            check2 = True
+            if len(source)<5:
+                for x in range(len(source)):
+                    if source[x]!=MI[p+x]:
+                        check2 = False
+                        break
+            else:
                 for x in range(1,5):
-                    if source.split(p)==MI.split(i+x):
-                        print("TAK")
+                    if source[x]!=MI[p+x]:
+                        check2 = False
+                        break
+            if check2 == True:
+                print("Mickiewicz")
+                exit()
     
+               
+                
+                
+                
---
+++ pis_dlazaw/test.py
@@ -0,0 +1,6 @@
+'''file = open('Mickiewicz.txt', 'r')
+MI = file.read()
+MI = MI.split()'''
+MI = input()
+MI = MI.split()
+print(MI)
